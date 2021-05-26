ba5153eda8967eec464cfb88826c22905280237f
Aitor <aitorpalomares@hotmail.com>
2021-05-26 12:48:05 +0100

    package installs

---
+++ src/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import logo from './logo.svg';
+import styles from './App.module.scss';
+
+const App = () => (
+  <div className={styles.app}>
+    <header>
+      demo
+    </header>
+  </div>
+);
+
+export default App;
