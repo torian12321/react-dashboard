5aa9767b5ab97255949cd5c07c113b4fd0e0a355
Aitor <aitorpalomares@hotmail.com>
2021-05-26 13:25:49 +0100

    linting

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import logo from './logo.svg';
 import Button from '../ui/components/Button';
 import Panel from '../ui/components/Panel';
 import styles from './App.module.scss';
