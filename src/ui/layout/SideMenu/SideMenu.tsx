e7756efe1a67e0bc4b945dae7feedfa923f7f79d
Aitor <aitorpalomares@hotmail.com>
2021-05-30 21:29:49 +0100

    mobile menu

--- src/ui/layout/SideMenu/SideMenu.tsx
+++ src/ui/layout/SideMenu/SideMenu.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classnames from 'classnames';
+// import classNames from 'classnames';
 import { NavLink } from 'react-router-dom';
 import { PATHS } from '../../../consts';
+import Btn from './SideMenu.Btn';
 import styles from './SideMenu.module.scss';
 
 const Link = ({ label, href, onClick }: any) => (
@@ -9,26 +12,40 @@ const Link = ({ label, href, onClick }: any) => (
       to={href}
       className={styles.link}
       activeClassName={styles.linkActive}
-      // onClick={onClick}
+      onClick={onClick}
     >
       {label}
     </NavLink>
   </li>
 );
 
-const SideMenu = () => (
-  <div className={styles.wrapper}>
-    <div className={styles.menu}>
-      <span className={styles.title}>
-        Dashboard
-      </span>
+const SideMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
 
-      <ul className={styles.menuLinksList}>
-        <Link href={PATHS.HOME.path} label={PATHS.DASHBOARD.label} />
-        <Link href={PATHS.SETTINGS.path} label={PATHS.SETTINGS.label} />
-      </ul>
+  return (
+    <div className={styles.wrapper}>
+      <Btn onClick={toggleMenu} />
+      <div className={classnames(
+        styles.menu,
+        isOpen && styles.isOpen
+      )}>
+        <span className={styles.title}>
+          Dashboard
+        </span>
+
+        <ul className={styles.menuLinksList}>
+          <Link href={PATHS.HOME.path} label={PATHS.DASHBOARD.label} onClick={closeMenu} />
+          <Link href={PATHS.SETTINGS.path} label={PATHS.SETTINGS.label} onClick={closeMenu} />
+        </ul>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default SideMenu;
